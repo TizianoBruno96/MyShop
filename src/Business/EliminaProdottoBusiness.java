78b00f87da9388fde0d96fc8f5b521f6320503af
Tiziano75775 <brntiziano9620@gmail.com>
2022-09-17 10:32:41 +0200

    bug fix

--- src/Business/EliminaProdottoBusiness.java
+++ src/Business/EliminaProdottoBusiness.java
@@ -1,11 +1,17 @@
 package Business;
 
+import DAO.FotoDAO;
+import DAO.Interfaces.IFotoDAO;
 import DAO.Interfaces.IProdottoDAO;
 import DAO.ProdottoDAO;
+import Model.Articoli.Foto;
+
+import java.util.ArrayList;
 
 public class EliminaProdottoBusiness {
     private static EliminaProdottoBusiness instance;
     IProdottoDAO prodottoDAO = ProdottoDAO.getInstance();
+    IFotoDAO fotoDAO = FotoDAO.getInstance();
 
     public static EliminaProdottoBusiness getInstance() {
         if (instance == null) {
@@ -16,6 +22,10 @@ public class EliminaProdottoBusiness {
 
     public void eliminaProdotto(int[] idProdotto) {
         for (int i : idProdotto) {
+            ArrayList<Foto> foto = fotoDAO.findByProdotto(i);
+            for (Foto f : foto) {
+                fotoDAO.removeByID(f.getIdFoto());
+            }
             prodottoDAO.removeByID(i);
         }
     }
